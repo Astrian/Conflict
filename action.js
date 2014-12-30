4c81a894708a60cdf73d33b253d1516fc2b6a4b2
Astrian <DataStructureHomework>
2014-12-30 14:00:07 +0800

    前进后退

--- action.js
+++ action.js
@@ -21,8 +21,18 @@ function popupAbout(){
 
 //进入文章详细页
 function intoArticle(articleID){ //articleID 为文章 ID
-    document.getElementById("list").style.display = "none";
-    document.getElementById("article-detil").style = "inherit";
     document.getElementById("articledetil-title").innerHTML = "文章名称，请从 RSS 中抓取";
     document.getElementById("articledetil-content").innerHTML = "文章内容，请从 RSS 中抓取";
+    document.getElementById("list").style.display = "none";
+    document.getElementById("article-detil").style = "inherit";
+    document.getElementById("mainmenu").style.display = "none";
+    document.getElementById("back").style.display="inherit"
+}
+
+//返回文章列表
+function backtolist(){ //articleID 为文章 ID
+    document.getElementById("article-detil").style.display = "none";
+    document.getElementById("list").style = "inherit";
+    document.getElementById("back").style.display = "none";
+    document.getElementById("mainmenu").style.display="inherit"
 }
