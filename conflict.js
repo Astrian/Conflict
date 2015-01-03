4cb72ba99031ca1e555ea209d20e6b45b98988b6
lsby <2451759073@qq.com>
2015-01-04 00:00:46 +0800

    清除多余的东西

--- conflict.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('index.htm', {
-        id:"Conflict",
-        'bounds': {
-            'width': 1200,
-            'height': 800
-        }
-    });
-});
