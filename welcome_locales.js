ef43c9857824cd3eb785a17ee43bb8818f38357d
Astrian <DataStructureHomework>
2015-01-10 23:22:54 +0800

    欢迎页多语言支援

---
+++ welcome_locales.js
@@ -0,0 +1,5 @@
+document.getElementById("message_hello").innerHTML = chrome.i18n.getMessage("initGuide_Hello");
+document.getElementById("message_welcome").innerHTML = chrome.i18n.getMessage("initGuide_welcome");
+document.getElementById("message_description").innerHTML = chrome.i18n.getMessage("initGuide_description");
+document.getElementById("message_whatsyourname").innerHTML = chrome.i18n.getMessage("whatsyourname");
+document.getElementById("message_continue").innerHTML = chrome.i18n.getMessage("initGuide_continue");
